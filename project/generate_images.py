1c8e01c3e7a261433e24dc0be4c2ddf0451496d3
ericxu0 <ericxu0@stanford.edu>
2019-06-01 22:24:24 -0700

    output images in pbrt-v3-scenes

--- project/generate_images.py
+++ project/generate_images.py
@@ -4,35 +4,39 @@ import subprocess
 import sys
 
 NUM_IMAGES = 1
-pbrt_filename = sys.argv[1]
+pbrt_dir = sys.argv[1]
 pbrt_scene = sys.argv[2]
 exr_filename_idx = int(sys.argv[3]) - 1
 lookat_idx = int(sys.argv[4]) - 1
 sample_idx = int(sys.argv[5]) - 1
 
 def save_pbrt_and_run(i, new_pbrt_file, pixel_samples):
+	extension = str(i) + "_" + str(pixel_samples)
+
+	idx = new_pbrt_file[exr_filename_idx].find(".exr")
+	new_pbrt_file[exr_filename_idx] = new_pbrt_file[exr_filename_idx][:idx] + extension + ".exr\"\n"
+
 	sample = new_pbrt_file[sample_idx].split()
 	sample[-1] = str(pixel_samples)
 	new_pbrt_file[sample_idx] = " ".join(sample) + "\n"
-	dir = pbrt_scene + "_data/"
-	path = dir + pbrt_scene + str(i) + "_" + str(pixel_samples) + ".pbrt"
+
+	path = pbrt_dir + pbrt_scene + extension + ".pbrt"
 	f = open(path, "w")
 	for line in new_pbrt_file:
 		f.write(line)
 	f.close()
+
 	subprocess.run(["pbrtBuild/pbrt", path])
-	subprocess.run(["mv", "feature_map.txt", dir + "feature_map" + str(i) + ".txt"])
-	subprocess.run(["mv", pbrt_scene + str(i) + ".exr", dir])
+	subprocess.run(["mv", "feature_map.txt", pbrt_dir + "feature_map" + extension + ".txt"])
+	subprocess.run(["mv", pbrt_scene +  + ".exr", pbrt_dir])
 
 pbrt_file = []
-with open(pbrt_filename, "r") as f:
+with open(pbrt_dir + pbrt_scene + ".pbrt", "r") as f:
 	for line in f:
 		pbrt_file.append(line)
 
 for i in range(NUM_IMAGES):
 	new_pbrt_file = copy.deepcopy(pbrt_file)
-	extension = new_pbrt_file[exr_filename_idx].find(".exr")
-	new_pbrt_file[exr_filename_idx] = new_pbrt_file[exr_filename_idx][:extension] + str(i) + ".exr\"\n"
 
 	lookat = new_pbrt_file[lookat_idx].split()
 	for j in range(1, 7):
